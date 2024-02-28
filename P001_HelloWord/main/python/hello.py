a149ffa1361b8fea61b98aedff20c8da4fe1a617
Witold Wróbel <wwrobel.mob@gmail.com>
2024-02-28 21:18:09 +0100

    first commit

---
+++ P001_HelloWord/main/python/hello.py
@@ -0,0 +1,10 @@
+'''
+Created on Feb 28, 2024
+
+@author: witek
+'''
+mark=int(input("Enter marks\n"))
+print("Your mark is ", (mark))
+print("apple","banana","orange",sep=",")
+print("Hello",end=" ")
+print("Word")
---
+++ P002_Literals/main/python/literals.py
@@ -0,0 +1,39 @@
+'''
+Created on Feb 28, 2024
+
+@author: witek
+'''
+full_name = "John Wick"
+print("Full name is ", full_name)
+num1=50
+num2=12
+print("Addition: ",num1+num2)
+print("Subtraction: ",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division: ",round(num1/num2,2))
+print("Integer division: ", num1//num2)
+print("Modulus: ",num1%num2)
+print("Exponential",2**4)
+
+first_name = "John"
+last_name = "Wick"
+
+print("String Concatenation: ", first_name + last_name)
+binary_num1 = 0b1010
+binary_num2 = 0b1111
+print("And Operation: ",end=" ")
+print(bin(binary_num1 & binary_num2))
+print("Or Operation: ",end=" ")
+print(bin(binary_num1 | binary_num2))
+
+print("num1 and num2 are equal")
+print("num1 and num2 are not equal")
+print("num1 is less than num2")
+print("num1 is less than or equal to num2")
+print("num1 is greater than num2")
+print("num1 is greater than or equal to num2")
+
+print("And Operator: ", True and True)
+print("Or Operation: " , True or False)
+print("Not Operator: ", not True)
+
