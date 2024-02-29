47a059d9c825dfea66a1f67b0290c23a5ed0c07a
Witold Wróbel <wwrobel.mob@gmail.com>
2024-02-29 22:38:57 +0100

    added project if-else

---
+++ P003_If-Else/main/python/condition.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 29, 2024
+
+@author: witek
+'''
+string1 = "John Hammond"
+string2 = "John Wick"
+if string1 == string2:
+    print("Strings are equal")
+    
+num1 = 50
+num2 = 100
+
+if num1 < num2:
+    print(num1, end=" ")
+    print("is less than", num2)
+else:
+    print(num1, end=" ")
+    print("is greater than or equal to", num2)
