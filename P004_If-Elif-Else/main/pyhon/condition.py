a89205de0f2ede0f2e11a49b26c4b8df189904c9
Witold Wróbel <wwrobel.mob@gmail.com>
2024-02-29 22:43:09 +0100

    added project if-elif-else

---
+++ P004_If-Elif-Else/main/pyhon/condition.py
@@ -0,0 +1,14 @@
+'''
+Created on Feb 29, 2024
+
+@author: witek
+'''
+marks=70
+if marks >= 90 and marks <= 100:
+    print("A grade")
+elif marks >= 80 and marks < 90:
+    print("B grade")
+elif marks >= 70 and marks < 80:
+    print("C grade")
+else:
+    print("F grade")
