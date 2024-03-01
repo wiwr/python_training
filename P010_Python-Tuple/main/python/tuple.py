566b72d224f4120586dcacb9cee2a7dc6a88a134
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-01 09:30:38 +0100

    Added project tuple

---
+++ P010_Python-Tuple/main/python/tuple.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 1, 2024
+
+@author: witek
+'''
+tuple1=(1,10,20,1)
+print(tuple1)
+print("Total occurences of 1 is ", tuple1.count(1))
+print("Length of tuple is",len(tuple1))
+print("Index position of 10 is ",tuple1.index(10,0))
+print("Value at index position 2 is ", tuple1[2])
