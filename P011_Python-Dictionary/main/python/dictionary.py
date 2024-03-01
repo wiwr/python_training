f7bccf603a602c5518c4cd23df247f382db712ad
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-01 10:51:38 +0100

    Added prject dictionary

---
+++ P011_Python-Dictionary/main/python/dictionary.py
@@ -0,0 +1,25 @@
+'''
+Created on Mar 1, 2024
+
+@author: witek
+'''
+dictionary1 = {
+     "name":"John Wick",
+     "age":40,
+     "city":"London",
+}
+print("Dictionary operation")
+print("Dictionary is",dictionary1)
+print("Dictionary keys are",dictionary1.keys())
+print("Dictionary values are",dictionary1.values())
+print("Dictionary items are",dictionary1.items())
+print("Looping though dictionary")
+for key,value in dictionary1.items():
+    print(key, " ", value)
+print("Checking if key country exists:",dictionary1.get("country","key not found"))
+print("Adding key country:")
+dictionary1["country"]="UK"
+print("Dictionary is ",dictionary1)
+print("Removing key contry:")
+dictionary1.pop("country","Key not found")
+print("Dictionary is ", dictionary1)
