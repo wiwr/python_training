8cb9458d8a328ca50f6a6074d9e0cbfdb00efae9
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-07 23:47:44 +0100

    exercise bytearray

---
+++ P031_ByteArrayOperation/main/python/bytearray.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 7, 2024
+
+@author: witek
+'''
+binary_data = b'Hello, World!\nThis is a test.\nPython is awesome!\n'
+buffer = bytearray(len(binary_data))
+buffer[:len(binary_data)] = binary_data
+print("Initial buffer contents:")
+print(buffer)
+with open("output.bin", "wb") as file:
+    file.write(buffer)
+    
+buffer.clear()
+
+with open("output.bin","rb") as file:
+    file.readinto(buffer)
+    content = file.read()
+    print(content)
