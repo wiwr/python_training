3208f5a4d96685671342b3b10bfdbf0e1802362b
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-17 21:54:34 +0100

    Tk label as object

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -2,10 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-myLabel1 = Label(root, text="Hello World!")
-myLabel2 = Label(root, text="My Name Is John Elder")
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="My Name Is John Elder").grid(row=1, column=1)
 
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=1)
+#myLabel1.grid(row=0, column=0)
+#myLabel2.grid(row=1, column=1)
 
 root.mainloop()
