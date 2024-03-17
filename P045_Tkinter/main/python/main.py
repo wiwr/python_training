681bd9c37fc59724ec173562cbcc36befd4429a7
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-17 22:01:42 +0100

    Tk input

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -2,12 +2,15 @@ from tkinter import *
 
 root = Tk()
 
+e = Entry(root, width=50)
+e.pack()
+
 def myClick():
-    myLabel = Label(root, text="Look! I clicked a Button!!")
+    myLabel = Label(root, text=e.get())
     myLabel.pack()
     
     
-myButton = Button(root, text="Click Me!", command=myClick, fg="blue", bg="red")
+myButton = Button(root, text="Enter Your Name", command=myClick, fg="blue", bg="red")
 myButton.pack()
 
 root.mainloop()
