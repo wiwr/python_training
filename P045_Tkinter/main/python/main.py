5a7860c6e51c0674fe73dfecde1beec492d25361
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-17 21:52:39 +0100

    Tk grid

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
+myLabel1 = Label(root, text="Hello World!")
+myLabel2 = Label(root, text="My Name Is John Elder")
 
-myLabel.pack()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
