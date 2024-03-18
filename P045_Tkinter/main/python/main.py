44089f32ebcc0e4b72cbe5fc2e3a7ec3866c5816
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-18 22:49:00 +0100

    Tk image

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -1,9 +1,14 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
-root.title("Learn To Code at Codemy.com")
+root.title("Codemy.com Image Viewer")
 
 #root.iconbitmap('ico.ico')
+
+my_img = ImageTk.PhotoImage(Image.open("hot.png"))
+my_label = Label(image=my_img)
+my_label.pack()
 
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.pack()
