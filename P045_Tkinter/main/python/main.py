0f1d01cd7ac7e988cc1b67b487e278794881b529
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-20 23:07:01 +0100

    Tk status bar

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -17,12 +17,16 @@ def forward(image_number):
         button_forward = Button(root, text=">>", state=DISABLED)
             
     button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
+    button_forward.grid(row=1, column=2, pady=10)
+    status = Label(root, text="Image " + str(image_number) + " of "+ str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)       
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+    
     
 def back(image_number):
     global my_label
     global button_forward
     global button_back
+    global status
     
     my_label.grid_forget()
     my_label = Label(image=image_list[image_number-1], width=1000, height=900)
@@ -34,7 +38,9 @@ def back(image_number):
         button_back = Button(root, text="<<", state=DISABLED)
             
     button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
+    button_forward.grid(row=1, column=2, pady=10)
+    status = Label(root, text="Image " + str(image_number) + " of "+ str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)           
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
     
     
 root = Tk()
@@ -48,7 +54,7 @@ my_img3 = ImageTk.PhotoImage(Image.open("images/image2.jpg"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/image3.jpg"))
 
 image_list = [my_img1, my_img2,my_img3,my_img4]
-            
+status = Label(root, text="Image 1 of "+ str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)           
             
 my_label = Label(image=my_img1, width=1000, height=900)
 my_label.grid(row=0, column=0, columnspan=3)
@@ -61,6 +67,6 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
-
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 root.mainloop()
