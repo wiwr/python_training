c0493805416a058018166b4dcef9f456f0e7048b
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-22 22:21:20 +0100

    Tk messagebox

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -1,40 +1,41 @@
 from tkinter import *
 from PIL import ImageTk,Image
+from tkinter import messagebox
+
     
 root = Tk()
 root.title("Codemy.com Image Viewer")
 
-r = IntVar()
-r.set("2")
 
-def clicked(value):
-    myLabel = Label(root,text=value)
-    myLabel.pack()
+def info():
+    messagebox.showinfo("This is my Popup!", "Hello Info Popups!")
 
-Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 3", variable=r, value=3, command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 4", variable=r, value=4, command=lambda: clicked(r.get())).pack()
 
-MODES = [
-    ("Pepperoni", "Pepperoni"),
-    ("Cheese", "Cheese"),
-    ("Mushroom", "Mushroom"),
-    ("Onion", "Onion")    
-    ]
+def warning():
+    messagebox.showwarning("This is my Popup!", "Hello Warning Popups!")
+    
+    
+def error():
+    messagebox.showerror("This is my Popup!", "Hello Error Popups!")
+    
+    
+def askquestion():
+    response = messagebox.askquestion("This is my Popup!", "Hello AskQuestion Popups!")
+    Label(root, text=response).pack()
+    
+def askokcancel():
+    response = messagebox.askokcancel("This is my Popup!", "Hello AskOkCancel Popups!")
+    Label(root, text=response).pack()
+    
+def askyesno():
+    response = messagebox.askyesno("This is my Popup!", "Hello AskYesNo Popups!")
+    Label(root, text=response).pack()
+                    
+Button(root, text="Popup - showinfo", command=info).pack()
+Button(root, text="Popup - showwarning", command=warning).pack()
+Button(root, text="Popup - showerror", command=error).pack()
+Button(root, text="Popup - askquestion", command=askquestion).pack()
+Button(root, text="Popup - askokcancel", command=askokcancel).pack()
+Button(root, text="Popup - askyesno", command=askyesno).pack()
 
-pizza = StringVar()
-pizza.set("Pepperoni")
-
-myLabel = Label(root, text=r.get())
-myLabel.pack()
-
-for text, mode in MODES:
-    Radiobutton(root, text=text, variable=pizza, value=mode).pack(anchor=W)
-
-pizzaLabel = Label(root, text=pizza.get())
-pizzaLabel.pack()
-
-myButton = Button(root, text="Click Me!", command=lambda: clicked(r.get()))
-myButton.pack()
 root.mainloop()
