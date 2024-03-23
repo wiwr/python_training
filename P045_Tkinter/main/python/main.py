ecd952136cc1c9c3ed0b938c4b5e998413b06359
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-23 22:01:16 +0100

    Tk checkbox

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -1,23 +1,23 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import filedialog
-
-def slide(hor):
-    my_label = Label(root, text=horizontal.get()).pack()
-    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()) )
     
 
 root = Tk()
 root.title("Codemy.com")
 root.geometry("400x400")
 
-vertical = Scale(root, from_=100, to=700)
-vertical.pack()
-
-horizontal = Scale(root, from_=100, to=700, orient=HORIZONTAL, command=slide)
-horizontal.pack()
+var = StringVar()
 
 
-my_button = Button(root, text="Click Me!!", command=slide).pack()
+def show():
+    myLabel = Label(root, text=var.get()).pack()
+
+checkButton = Checkbutton(root, text="Check this box, I dare you!", variable=var, onvalue="ON", offvalue="OFF")
+checkButton.deselect()
+checkButton.pack()
+
+
+myButton = Button(root, text="Show Selection", command=show).pack()
 
 root.mainloop()
