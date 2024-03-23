f768c87c6db4bc4576f75ff203d6f2a3e5b9f24c
Witold Wróbel <wwrobel.mob@gmail.com>
2024-03-23 21:52:23 +0100

    Tk slider

--- P045_Tkinter/main/python/main.py
+++ P045_Tkinter/main/python/main.py
@@ -2,18 +2,22 @@ from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import filedialog
 
+def slide(hor):
+    my_label = Label(root, text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get()) + "x" + str(vertical.get()) )
+    
 
 root = Tk()
 root.title("Codemy.com")
+root.geometry("400x400")
+
+vertical = Scale(root, from_=100, to=700)
+vertical.pack()
+
+horizontal = Scale(root, from_=100, to=700, orient=HORIZONTAL, command=slide)
+horizontal.pack()
 
 
+my_button = Button(root, text="Click Me!!", command=slide).pack()
 
-def open():
-    global my_image
-    root.filename = filedialog.askopenfilename(initialdir="", title="Select A File", filetypes=(("png files", "*.png"),("all files", "*")))
-    my_label = Label(root, text=root.filename).pack()
-    my_image = ImageTk.PhotoImage(Image.open(root.filename))
-    my_image_label = Label(image=my_image).pack()
-
-my_button = Button(root, text="Open File", command=open).pack()
 root.mainloop()
